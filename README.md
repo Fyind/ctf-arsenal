c8e7b67fc07171b832b5c52111bbf4753b990daa
Yi Fan <yi.fan@tum.de>
2025-02-04 16:54:42 +0100

    add install

---
+++ README.md
@@ -0,0 +1,64 @@
+
+
+网站地址： https://cr4zyp1x3l.netlify.app/
+
+## 安装
+
+### 一键安装
+
+``` shell
+py install.py
+```
+
+### node
+
+https://nodejs.org/en/
+
+### git
+
+https://git-scm.com/downloads
+
+### hexo
+
+``` shell
+npm install -g hexo-cli
+```
+
+### ShokaX
+
+``` shell
+npm install shokax-cli --location=global
+SXC install shokaX
+```
+
+### 图片
+
+``` shell
+npm install hexo-asset-image --save
+```
+
+
+
+
+
+
+
+## 写新文章
+
+``` shell
+hexo new post "<name>"
+```
+
+写好了先生成
+
+``` shell
+hexo clean && hexo g
+```
+
+然后push就可以了
+
+reference:
+
+ https://jzhzh.com/2022/12/18/netlify/
+
+https://fyind.github.io/My-First-Blog/
---
+++ install.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+
+a = subprocess.check_output("node -v").decode()
+if a[0] != 'v':
+    print("Node JS not installed. Go to: https://nodejs.org/en/")
+    exit()
+a = subprocess.check_output("git -v").decode()
+if a[:11] != 'git version':
+    print("Git not installed. Go to: https://git-scm.com/downloads")
+    exit()
+
+os.system("npm install -g hexo-cli")
+os.system("npm install shokax-cli --location=global")
+os.system("SXC install shokaX")
+os.system("npm install hexo-asset-image --save")
+
+with open("modified_code.js","r") as f:
+    text = f.read()
+
+with open("node_modules/hexo-asset-image/index.js","w") as f:
+    f.write(text)
+with open("node_modules/hexo-theme-shokax/scripts/filters/post.js","w") as f:
+    f.write(text)
+
+os.system("hexo g")
+print("Successfuly installed")
